f8d29e1cc6786fdfa502bd5c7f4d842bc73d48db
AkashGobalarajah <gobalarajah@gmail.com>
2023-11-14 16:16:55 +0100

    check

--- logic.js
+++ logic.js
@@ -1,1 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jokeContainer = document.getElementById('joke-container');
 
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'J' || event.key === 'j') {
+            fetchJoke();
+        }
+    });
+
+    jokeContainer.addEventListener('mouseover', fetchJoke); // Changed 'mousehover' to 'mouseover'
+
+    function fetchJoke() {
+        fetch('https://official-joke-api.appspot.com/random_joke')
+            .then(response => response.json())
+            .then(joke => {
+                jokeContainer.innerHTML = `<p>${joke.setup} <br> ${joke.punchline}</p>`; // This replaces the current joke with a new one
+            })
+            .catch(error => console.log('Error:', error));
+    }
+});
