b9c8af313d1d519fe7c4dd9e982481b257064b84
AkashGobalarajah <gobalarajah@gmail.com>
2023-11-16 22:13:46 +0100

    testings

--- logic.js
+++ logic.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    jokeContainer.addEventListener('mouseover', fetchJoke); // Changed 'mousehover' to 'mouseover'
+    jokeContainer.addEventListener('click', fetchJoke); // Changed from 'mouseover' to 'click'
 
     function fetchJoke() {
         fetch('https://official-joke-api.appspot.com/random_joke')
             .then(response => response.json())
             .then(joke => {
-                jokeContainer.innerHTML = `<p>${joke.setup} <br> ${joke.punchline}</p>`; // This replaces the current joke with a new one
+                jokeContainer.innerHTML = `<p>${joke.setup} <br> ${joke.punchline}</p>`;
             })
             .catch(error => console.log('Error:', error));
     }
